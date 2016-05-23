10ebe04e25487da9b5c1d5a44d19dc5da352af1e
Samuel Padgett <spadgett@redhat.com>
2016-05-23 19:38:47 -0400

    Overview updates
    - Remove incorrect metadata key from dependencies annotation
    - Show recent failed pipelines on overview

--- app/scripts/directives/resources.js
+++ app/scripts/directives/resources.js
@@ -39,7 +39,7 @@ angular.module('openshiftConsole')
         deploymentConfigs: '=',
         deployments: '=',
         replicationControllers: '=',
-        runningPipelines: '=',
+        recentPipelines: '=',
         pipelinesByDeployment: '=',
         podsByDeployment: '='
       },
