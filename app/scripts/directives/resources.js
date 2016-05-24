3ea7fff605754527b7cdbe395f0532ff3947c097
Samuel Padgett <spadgett@redhat.com>
2016-05-24 16:09:37 -0400

    Add route warnings to new overview

--- app/scripts/directives/resources.js
+++ app/scripts/directives/resources.js
@@ -36,6 +36,7 @@ angular.module('openshiftConsole')
       scope: {
         service: '=',
         routes: '=',
+        routeWarnings: '=',
         deploymentConfigs: '=',
         deployments: '=',
         replicationControllers: '=',
