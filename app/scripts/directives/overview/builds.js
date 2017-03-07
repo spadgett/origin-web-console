f87c1893a5323a8623b6b1704ed87d31cbfe0d79
Samuel Padgett <spadgett@redhat.com>
2017-03-07 14:19:28 -0500

    Show completed build logs for 10 minutes

--- app/scripts/directives/overview/builds.js
+++ app/scripts/directives/overview/builds.js
@@ -1,12 +1,52 @@
 'use strict';
 
 angular.module('openshiftConsole').component('overviewBuilds', {
+  controller: [
+    '$filter',
+    OverviewBuilds
+  ],
   controllerAs: 'overviewBuilds',
   bindings: {
     buildConfigs: '<',
     recentBuildsByBuildConfig: '<',
     context: '<',
-    showLog: '<'
+    hideLog: '<'
   },
   templateUrl: 'views/overview/_builds.html'
 });
+
+function OverviewBuilds($filter) {
+  var canIViewLogs;
+  var canI = $filter('canI');
+
+  this.$onInit = function() {
+    canIViewLogs = canI('builds/log', 'get');
+  };
+
+  this.showLogs = function(build) {
+    if (this.hideLog) {
+      return false;
+    }
+
+    if (!canIViewLogs) {
+      return false;
+    }
+
+    if (!_.get(build, 'status.startTimestamp')) {
+      return false;
+    }
+
+    if (_.get(build, 'status.phase') !== 'Complete') {
+      return true;
+    }
+
+    // Has the build completed recently?
+    var completed = _.get(build, 'status.completionTimestamp');
+    if (!completed) {
+      return false;
+    }
+
+    var tenMinutesAgo = moment().subtract(10, 'm');
+    return moment(completed).isAfter(tenMinutesAgo);
+  };
+}
