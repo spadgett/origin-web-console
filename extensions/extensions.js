1e67cc495492ff8df0fd7a00a5220fba30e230c2
Samuel Padgett <spadgett@redhat.com>
2017-03-20 17:03:01 -0400

    Custom Header Example

--- extensions/extensions.js
+++ extensions/extensions.js
@@ -9,3 +9,21 @@
  *
  * You can modify this file to test extensions in a development environment.
  */
+angular.module('openshiftConsole')
+  .run(function($rootScope, $compile, $routeParams, $timeout) {
+    $rootScope.$on('$viewContentLoaded', function() {
+      if ($routeParams.view === 'chromeless') {
+        $timeout(function() {
+          $('.navbar').css('margin-top', '0');
+        });
+        return;
+      }
+      $('.top-header').before($compile('<div id="custom-header" class="custom-header"></div>')($rootScope));
+    });
+  })
+  .directive('customHeader', function() {
+    return {
+      restrict: 'C',
+      template: '<div>Hello World</div>'
+    };
+  });
