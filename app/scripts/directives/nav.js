235a672c33f35d9f8fab4c50e7e2cfc8ee3534c9
sg00dwin <sgoodwin@redhat.com>
2017-08-14 09:45:22 -0400

    Use Patternfly vertical navigation and add project bar

--- app/scripts/directives/nav.js
+++ app/scripts/directives/nav.js
@@ -1,38 +1,54 @@
 'use strict';
 
 angular.module('openshiftConsole')
-  .directive('sidebar', function($location, $filter, Constants) {
+  .directive('sidebar',
+    function(
+      $location,
+      $filter,
+      $timeout,
+      $rootScope,
+      Constants,
+      HTMLService) {
     var canI = $filter('canI');
     var itemMatchesPath = function(item, path) {
       return (item.href === path) || _.some(item.prefixes, function(prefix) {
-            return _.startsWith(path, prefix);
-          });
+        return _.startsWith(path, prefix);
+      });
     };
     return {
       restrict: 'E',
       templateUrl: 'views/_sidebar.html',
       controller: function($scope) {
-        var path = $location.path().replace("/project/" + $scope.projectName, "");
-        $scope.activeSecondary;
-        $scope.navItems = Constants.PROJECT_NAVIGATION;
-        $scope.activePrimary = _.find($scope.navItems, function(primaryItem) {
-          if (itemMatchesPath(primaryItem, path)) {
-            $scope.activeSecondary = null;
-            return true;
-          }
+        var path;
+        var hoverDelay = 300;
+        var hideDelay = hoverDelay + 200;
 
-          // Check if there is a secondary nav item that is active
-          return _.some(primaryItem.secondaryNavSections, function(secondarySection) {
-            var activeSecondary = _.find(secondarySection.items, function(secondaryItem) {
-              return itemMatchesPath(secondaryItem, path);
-            });
-            if (activeSecondary) {
-              $scope.activeSecondary = activeSecondary;
+        $scope.navItems = Constants.PROJECT_NAVIGATION;
+        $scope.sidebar = {};
+
+        var updateActive = function() {
+          $scope.activePrimary = _.find($scope.navItems, function(primaryItem) {
+            path = $location.path().replace("/project/" + $scope.projectName, "");
+            if (itemMatchesPath(primaryItem, path)) {
+              $scope.activeSecondary = null;
               return true;
             }
-            return false;
+
+            // Check if there is a secondary nav item that is active
+            return _.some(primaryItem.secondaryNavSections, function(secondarySection) {
+              var activeSecondary = _.find(secondarySection.items, function(secondaryItem) {
+                return itemMatchesPath(secondaryItem, path);
+              });
+              if (activeSecondary) {
+                $scope.activeSecondary = activeSecondary;
+                return true;
+              }
+              return false;
+            });
           });
-        });
+        };
+        updateActive();
+        $scope.$on('$routeChangeSuccess', updateActive);
 
         $scope.navURL = function(href) {
           if (!href) {
@@ -55,10 +71,90 @@ angular.module('openshiftConsole')
 
           return userCan;
         };
+
+        $scope.onMouseEnter = function(primaryItem) {
+          if (_.isEmpty(primaryItem.secondaryNavSections)) {
+            return;
+          }
+
+          if (primaryItem.mouseLeaveTimeout) {
+            $timeout.cancel(primaryItem.mouseLeaveTimeout);
+            primaryItem.mouseLeaveTimeout = null;
+          }
+
+          primaryItem.mouseEnterTimeout = $timeout(function() {
+            primaryItem.isHover = true;
+            primaryItem.mouseEnterTimeout = null;
+            $scope.sidebar.secondaryOpen = true;
+          }, hoverDelay);
+        };
+
+        $scope.onMouseLeave = function(primaryItem) {
+          if (_.isEmpty(primaryItem.secondaryNavSections)) {
+            return;
+          }
+
+          if (primaryItem.mouseEnterTimeout) {
+            $timeout.cancel(primaryItem.mouseEnterTimeout);
+            primaryItem.mouseEnterTimeout = null;
+          }
+
+          primaryItem.mouseLeaveTimeout = $timeout(function() {
+            primaryItem.isHover = false;
+            primaryItem.mouseLeaveTimeout = null;
+            $scope.sidebar.secondaryOpen = _.some($scope.navItems, function(primaryItem) {
+              return primaryItem.isHover && !_.isEmpty(primaryItem.secondaryNavSections);
+            });
+          }, hideDelay);
+        };
+
+        $scope.collapseMobileSecondary = function(primaryItem, event) {
+          primaryItem.mobileSecondary = false;
+          event.stopPropagation();
+        };
+
+        var checkMobile = function() {
+          return HTMLService.isWindowBelowBreakpoint(HTMLService.WINDOW_SIZE_SM);
+        };
+
+        $scope.isMobile = checkMobile();
+
+        var onResize = _.throttle(function() {
+          var isMobile = checkMobile();
+          if (isMobile !== $scope.isMobile) {
+            $scope.$evalAsync(function() {
+              $scope.isMobile = isMobile;
+              if (isMobile) {
+                _.set($rootScope, 'nav.collapsed', false);
+              } else {
+                _.set($rootScope, 'nav.showMobileNav', false);
+                _.each($scope.navItems, function(primaryItem) {
+                  primaryItem.mobileSecondary = false;
+                });
+              }
+            });
+          }
+        }, 50);
+        $(window).on('resize.verticalnav', onResize);
+
+        $scope.$on('$destroy', function() {
+          $(window).off('.verticalnav');
+        });
       }
     };
   })
-  .directive('projectHeader', function($timeout, $location, $filter, ProjectsService, projectOverviewURLFilter, Constants) {
+  .directive('oscHeader',
+    function(
+      $filter,
+      $location,
+      $rootScope,
+      $routeParams,
+      $timeout,
+      AuthService,
+      AuthorizationService,
+      Constants,
+      ProjectsService,
+      projectOverviewURLFilter) {
 
     // cache these to eliminate flicker
     var projects = {};
@@ -69,9 +165,19 @@ angular.module('openshiftConsole')
 
     return {
       restrict: 'EA',
-      templateUrl: 'views/directives/header/project-header.html',
+      templateUrl: 'views/directives/header/header.html',
       link: function($scope, $elem) {
         var MAX_PROJETS_TO_DISPLAY = 100;
+
+        $scope.toggleNav = function() {
+          var collapsed = _.get($rootScope, 'nav.collapsed');
+          _.set($rootScope, 'nav.collapsed', !collapsed);
+        };
+
+        $scope.toggleMobileNav = function() {
+          var showMobileNav = _.get($rootScope, 'nav.showMobileNav');
+          _.set($rootScope, 'nav.showMobileNav', !showMobileNav);
+        };
 
         $scope.closeOrderingPanel = function() {
           _.set($scope, 'ordering.panelName', "");
@@ -85,32 +191,9 @@ angular.module('openshiftConsole')
         var options = [];
 
         var updateOptions = function() {
-          var project = $scope.project || {};
-          $scope.context = {
-            namespace: $scope.projectName
-          };
           var name = $scope.projectName;
-          var isRealProject = project.metadata && project.metadata.name;
-
-          // If we don't have a name or a real project, nothing to do yet.
-          if (!name && !isRealProject) {
-            return;
-          }
-
           if (!name) {
-            name = project.metadata.name;
-          }
-
-          if (!isRealProject) {
-            project = {
-              metadata: {
-                name: name
-              }
-            };
-          }
-
-          if(!projects[name]) {
-            projects[name] = project;
+            return;
           }
 
           var makeOption = function(project, skipUniqueCheck) {
@@ -145,13 +228,65 @@ angular.module('openshiftConsole')
           select.selectpicker('refresh');
         };
 
+        var updateProjects = function() {
+          return ProjectsService.list().then(function(items) {
+            projects = items.by("metadata.name");
+          });
+        };
 
-        ProjectsService.list().then(function(items) {
-          projects = items.by("metadata.name");
-          updateOptions();
-        });
+        var init = function() {
+          updateProjects().then(function() {
+            $scope.project = projects[ $routeParams.project ];
+            updateOptions();
+          });
 
-        updateOptions();
+          $scope.$on('$routeChangeSuccess', function() {
+            var projectName = $routeParams.project;
+            if ($scope.projectName === projectName) {
+              // The project hasn't changed.
+              return;
+            }
+            if($routeParams.view === "chromeless") {
+              $scope.chromeless = true;
+            }
+
+            if (projectName && !$scope.chromeless) {
+              $('body').addClass('has-project-bar');
+              // Check if the user can add to project after switching projects.
+              // Assume false until the request completes.
+              $scope.canIAddToProject = false;
+              // Make sure we have project rules before we check canIAddToProject or we get the wrong value.
+              // FIXME: We are not requesting this twice, here and in ProjectService
+              // FIXME: AuthorizationService should not cache the wrong value before the rules load
+              AuthorizationService.getProjectRules(projectName).then(function() {
+                // Make sure the user hasn't switched projects while the request was still in flight.
+                if ($scope.projectName !== projectName) {
+                  return;
+                }
+
+                $scope.canIAddToProject = AuthorizationService.canIAddToProject(projectName);
+              });
+            } else {
+              $('body').removeClass('has-project-bar');
+            }
+
+            // TODO: relist projects? Probably better not to for cluster admins
+            // that might have hundreds of projects. But we would want to update
+            // the dropdown at least when the user navigates to the project list,
+            // and we have an updated list.
+            updateProjects().then(function() {
+              $scope.projectName = projectName;
+              $scope.project = _.get(projects, [ projectName ]);
+              updateOptions();
+            });
+          });
+        };
+
+        if (AuthService.isLoggedIn()) {
+          init();
+        } else {
+          AuthService.onUserChanged(init);
+        }
 
         select
           .selectpicker({
@@ -165,12 +300,6 @@ angular.module('openshiftConsole')
               $location.url(newURL);
             });
           });
-
-        $scope.$on('project.settings.update', function(event, data) {
-          projects[data.metadata.name] = data;
-          updateOptions();
-        });
-
       }
     };
   })
@@ -188,13 +317,6 @@ angular.module('openshiftConsole')
       }
     };
   })
-  .directive('projectPage', function() {
-    return {
-      restrict: 'E',
-      transclude: true,
-      templateUrl: 'views/_project-page.html'
-    };
-  })
   .directive('navbarUtility', function() {
     return {
       restrict: 'E',
@@ -210,13 +332,6 @@ angular.module('openshiftConsole')
       restrict: 'E',
       transclude: true,
       templateUrl: 'views/directives/header/_navbar-utility-mobile.html'
-    };
-  })
-  .directive('defaultHeader', function() {
-    return {
-      restrict: 'E',
-      transclude: true,
-      templateUrl: 'views/directives/header/default-header.html'
     };
   })
   // TODO: rename this :)
